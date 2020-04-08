31967fb7226ae1aba28510d1658ff5d66d5d3aea
Luca Crema <lc.crema@hotmail.com>
2020-04-08 19:31:47 +0200

    refactor(PostgreAdapter): cleaned constructor from loading configurations from file - Moved config.json load from class to main script + Constructor documentation

--- database/posgresql_adapter.py
+++ database/posgresql_adapter.py
@@ -1,5 +1,5 @@
 from database.database_adapter import DatabaseAdapter, DatabaseData, DatabaseQuery
-from config import Config
+
 import json
 import psycopg2
 from psycopg2.extras import execute_values
@@ -10,19 +10,23 @@ class PosgreSQLAdapter(DatabaseAdapter):
     Database adapter for postgreSQL
     '''
 
-    def __init__(self):
+    def __init__(self, username: str, password: str, host: str, port: str = "5432"):
+        '''
+        Initialises postgreSQL connection.
+
+        Parameters:
+            username : str
+            password : str
+            host : str
+                IP or URL of the DB
+            port : str
+                Port where the DB is hosted
+        '''
         try:
             print("Trying to connect to PGSQL Database")
-            config = Config()
-            self.connection = psycopg2.connect(user=config.get_config("postgre_username"),
-                                               password=config.get_config(
-                                                   "postgre_password"),
-                                               host=config.get_config(
-                                                   "postgre_host"),
-                                               port="5432")
+            self.connection = psycopg2.connect(
+                user=username, password=password, host=host, port=port)
             self.cursor = self.connection.cursor()
-            #self.cursor.execute("SELECT tablename FROM pg_catalog.pg_tables;")
-            #print(self.cursor.fetchall())
             print("Connected to PGSQL")
 
         except (Exception, psycopg2.Error) as error:
@@ -43,13 +47,17 @@ class PosgreSQLAdapter(DatabaseAdapter):
             self.__create_table(data.category)
 
         if(type(data.values) == list):
-            data_json_list = [(json.dumps(element),) for element in data.values]
-            execute_values(self.cursor, "INSERT INTO {} (data_json) VALUES %s".format(data.category), data_json_list)
+            data_json_list = [(json.dumps(element),)
+                              for element in data.values]
+            execute_values(self.cursor, "INSERT INTO {} (data_json) VALUES %s".format(
+                data.category), data_json_list)
             self.connection.commit()
             print("Upload completed")
         elif(type(data.values) == dict):
-            self.cursor.execute("INSERT INTO {} (data_json) VALUES %s".format(data.category), (data.values,))
+            self.cursor.execute("INSERT INTO {} (data_json) VALUES %s".format(
+                data.category), (data.values,))
             self.connection.commit()
+            print("Upload completed")
         else:
             raise ValueError("Data value not a list or a dict")
 
@@ -76,7 +84,8 @@ class PosgreSQLAdapter(DatabaseAdapter):
                 Name of the table to create.
         '''
         print("Creating pgSQL DB {}".format(table_name))
-        self.cursor.execute("CREATE TABLE {} (id BIGSERIAL PRIMARY KEY, data_json JSON NOT NULL);".format(table_name))
+        self.cursor.execute(
+            "CREATE TABLE {} (id BIGSERIAL PRIMARY KEY, data_json JSON NOT NULL);".format(table_name))
         self.connection.commit()
 
     def __table_exists(self, table_name: str):
@@ -89,7 +98,8 @@ class PosgreSQLAdapter(DatabaseAdapter):
         Returns:
             True if table exists, False otherwise.
         '''
-        self.cursor.execute("SELECT tablename FROM pg_catalog.pg_tables WHERE tablename = %s;", (table_name,))
+        self.cursor.execute(
+            "SELECT tablename FROM pg_catalog.pg_tables WHERE tablename = %s;", (table_name,))
         return len(self.cursor.fetchall()) > 0
 
     def close(self):
--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 from importer.yahoo_json_data_importer import YahooJSONDataImporter
 from database.posgresql_adapter import PosgreSQLAdapter
 from pathlib import Path
+from config import Config
 
 if __name__ == '__main__':
-    database_adapter = PosgreSQLAdapter()
+    config = Config()
+    database_adapter = PosgreSQLAdapter(config.get_config("postgre_username"), config.get_config("postgre_password"), config.get_config("postgre_host"))
     file_data_importer = YahooJSONDataImporter(database_adapter)
     file_data_importer.from_file(Path("data/AA_27-03-20_to_20-03-20.json"))
