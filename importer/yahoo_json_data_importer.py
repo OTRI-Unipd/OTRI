9a7eafc4c586142d83cb8fc16020f56a1591b08d
Luca Crema <lc.crema@hotmail.com>
2020-04-15 17:08:39 +0200

    fix(yahoo_json_importer): removed double for loop + added __prepare_data() method ~ __fix_atom_datetime() now accepts one single atom ~ __add_metadata_to_atom() now accepts one single atom

--- importer/yahoo_json_data_importer.py
+++ importer/yahoo_json_data_importer.py
@@ -17,29 +17,52 @@ class YahooJSONDataImporter(JSONDataImporter):
                 A dictionary of key-values, should be loaded using json.load(filepath) from a file or DataFrame.__to_json(orient="table", indent=4)
         '''
         new_data = self.__to_lowercase_keys(json_file_contents)
-        atoms = self.__add_metadata_to_atoms(new_data['metadata'], new_data['data'])
-        atoms = self.__fix_atoms_datetime(atoms)
+        atoms = self.__prepare_data(new_data)
         self.database.write(DatabaseData("atoms_b",atoms))
 
-    def __fix_atoms_datetime(self, atoms : list):
-        for atom in atoms:
-            atom['datetime'] = datetime.strptime(atom['datetime'],"%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-        return atoms
-
-    def __add_metadata_to_atoms(self, metadata : dict, atoms : list):
+    def __prepare_data(self, data : dict):
         '''
-        Adds "source" : source to all dictionary elements of the given list
+        Prepares atoms to be ready to be written into DB.
 
         Parameters:
-            source : str
-                Name of the source where the data has been imported from
-            atoms : list
-                List of atoms to import
+            data : dict
+                JSON document containing atoms and metadata.
+                Atoms must be contained in data['data'].
+                Metadata must be contained in data['metadata']
+        Returns:
+            list of atoms ready for DB with metadata and fixed date format.
         '''
-        for atom in atoms:
-            for key,value in metadata.items():
-                atom[key] = value
-        return atoms
+        for atom in data['data']:
+            self.__add_metadata_to_atom(data['metadata'], atom)
+            self.__fix_atom_datetime(atom)
+        return data['data']
+
+    def __fix_atom_datetime(self, atom : dict):
+        '''
+        Changes yahoo UTC datetime to a 'Y-m-d H:m:s.ms' format
+
+        Parameters:
+            atom : dict
+                An atom from the list of atoms.
+        Returns:
+            The atom with the modified date.
+        '''
+        atom['datetime'] = datetime.strptime(atom['datetime'],"%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        return atom
+
+    def __add_metadata_to_atom(self, metadata : dict, atom : dict):
+        '''
+        Adds every entry of metadata to the atom.
+
+        Parameters:
+            metadata : dict
+                All kind of metadata to add to the atom.
+            atom : dict
+                Atom from the list of atoms.
+        '''
+        for key,value in metadata.items():
+            atom[key] = value
+        return atom
 
     def __to_lowercase_keys(self, data_dict : dict):
         '''
