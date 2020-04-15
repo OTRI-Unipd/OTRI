743091e84762ccdb0d4453510c7b55067fb00d14
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-04-15 14:05:44 +0200

    feat (json_key_handler.py): Generalized methods + Added generic method "apply_deep" taking in both a data dict/list and a function to apply to the keys + Added generic versions of apply_deep for dict and list - Removed _lower_keys_dict and _lower_keys_list ~ lower_all_keys_deep now simply calls apply_deep with a lambda that lowers the parameter if its a string or leaves it untouched.

--- importer/json_key_handler.py
+++ importer/json_key_handler.py
@@ -2,14 +2,74 @@ import json
 
 LOWER_ERROR = "Only dictionaries and lists can be modified by this method."
 
-def lower_all_keys_deep(data):
+def apply_deep(data, fun):
     '''
-    Renames all the keys to be lower case.
+    Applies fun to all keys in data.
     The method is recursive and applies as deep as possible in the json nest. 
 
     Parameters:
         data : dict | list
             Data to modify, must be either a dictionary or a list of dictionaries.
+        fun : function | lambda
+            Function to apply to each key, must take the key as its single parameter.
+    Returns:
+        A copy of the dict or list with the modified keys, and where all nested dicts and list
+        will receive the same treatment it will return the original
+        object (not a copy) if no operation could be applied, for example when:
+        - data is not a list or dict
+        - data is a list of non dict items
+        - data is not a list that contains dicts at any nesting level
+        ...
+    '''
+    if type(data) == dict:
+        return _apply_deep_dict(data, fun)
+    if type(data) == list:
+        return _apply_deep_list(data, fun)
+    return data
+
+def _apply_deep_dict(data : dict, fun):
+    '''
+    Applies fun to all keys in a dictionary and all nested items.
+
+    Parameters:
+        data : dict
+            Data to modify, must be a dictionary.
+        fun : function | lambda
+            Function to apply to each key, must take the key as its single parameter.
+    Returns:
+        A copy of the dict with the renamed keys, where all values have been replaced by copies of
+        their original if apply_deep(value, fun) was appliable.
+    '''
+    new_data = dict()
+    for key, value in data.items():
+        new_key = fun(key)
+        new_data[new_key] = apply_deep(value, fun)
+    return new_data
+
+def _apply_deep_list(data: list, fun):
+    '''
+    Applies fun to all keys in each item of the list, if appliable.
+
+    Parameters:
+        data : list
+            Data to modify, should be a list, but can be a tuple.
+        fun : function | lambda
+            Function to apply to each key, must take the key as its single parameter.
+    Returns:
+        A copy of the list, where each item got its keys modified through apply_deep(item, fun) if appliable.
+    '''
+    return [apply_deep(item, fun) for item in data]
+
+def lower_all_keys_deep(data):
+    '''
+    Renames all the keys in a dict object to be lower case.
+    The method is recursive and applies as deep as possible in the json nest. 
+
+    Parameters:
+        data : dict | list
+            Data to modify, must be either a dictionary or a list of dictionaries.
+            Technically meant to work with a json object dictionary, should work with
+            any dictionary. In any case, only string keys will be modified.
     Returns:
         A copy of the dict or list with the renamed keys, and where all nested dicts and list
         will receive the same treatment it will return the original
@@ -19,42 +79,7 @@ def lower_all_keys_deep(data):
         - data is not a list that contains dicts at any nesting level
         ...
     '''
-    if type(data) == dict:
-        return _lower_keys_dict(data)
-    if type(data) == list:
-        return _lower_keys_list(data)
-    return data
-
-
-def _lower_keys_dict(data: dict):
-    '''
-    Lowers all keys in a dictionary and all nested items.
-
-    Parameters:
-        data : dict
-            Data to modify, must be a dictionary.
-    Returns:
-        A copy of the dict with the renamed keys, where all values have been replaced by copies of
-        their original if lower_all_keys(value) was appliable.
-    '''
-    new_data = dict()
-    for key, value in data.items():
-        lower_key = key.lower()
-        new_data[lower_key] = lower_all_keys_deep(value)
-    return new_data
-
-
-def _lower_keys_list(data: list):
-    '''
-    Lowers all keys in each item of the list, if appliable.
-
-    Parameters:
-        data : list
-            Data to modify, should be a list, but can be a tuple.
-    Returns:
-        A copy of the list, where each item got its keys lowered through lower_all_keys(item) if appliable.
-    '''
-    return [lower_all_keys_deep(item) for item in data]
+    return apply_deep(data, lambda s : s.lower() if type(s) == str else s)
 
 # Just some test code
 if __name__ == "__main__":
