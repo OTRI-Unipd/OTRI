4f6577deabaa8fea4536c939ce43e611b74393c1
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-04-28 21:10:37 +0200

    feat (validation.py): + Added AtomValidatorInterface, weak interface for future atom validators + Added BaseValidator implementing above interface as generically as possible
    feat (AtomValidatorInterface):
    Class methods:
    + get_checks() returns the previously given checks. The list of checks is private.
    + add_checks(Sequence[Callable]) adds the given checks to the list
    + remove_checks(Sequence[Callable]), take a wild guess.
    + validate(Sequence[Any]), validates the given sequence with the stored checks, returning a map[check, result]

---
+++ validation/validation.py
@@ -0,0 +1,109 @@
+from typing import Sequence, Callable, Mapping, List, Any
+
+
+class AtomValidatorInterface():
+    '''
+    Interface defining basic behaviour of a standard AtomValidator Object.
+    '''
+
+    def __init__(self):
+        super().__init__()
+
+    def get_checks(self) -> Sequence[Callable]:
+        '''
+        Returns:
+            The Sequence of checks this Validator would apply on a given list
+            of atoms.
+        '''
+        pass
+
+    def add_checks(self, *args: Sequence[Callable]):
+        '''
+        Add one or more checks to use when validating.
+
+        Parameters:
+            *args : Sequence[Callable]
+                A list of functions or lambdas that expect the list of
+                atoms as a parameter.
+        '''
+        pass
+
+    def remove_checks(self, *args: Sequence[Callable]):
+        '''
+        Remove one or more checks from the ones to apply
+        when validating.
+
+        Parameters:
+            *args : Sequence[Callable]
+                A list of functions or lambdas. They will not
+                be removed if not currently present.
+        '''
+        pass
+
+    def validate(self, atoms: Sequence) -> Mapping[Callable, Any]:
+        '''
+        Applies all previously provided checks to the given list.
+
+        Parameters:
+            atoms : Sequence
+                The sequence of atoms to validate, will be passed
+                to all previously given checks.
+        Returns:
+            A Mapping of each check to its result.
+        '''
+        pass
+
+
+class BaseValidator(AtomValidatorInterface):
+    '''
+    A simple class performing the given checks, with no other particular logic.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.__checks = list()
+
+    def get_checks(self) -> List[Callable]:
+        '''
+        Returns:
+            The list of checks this Validator would apply on a given list
+            of atoms.
+        '''
+        return self.__checks
+
+    def add_checks(self, *args: Sequence[Callable]):
+        '''
+        Add one or more checks to use when validating.
+
+        Parameters:
+            *args : Sequence[Callable]
+                A list of functions or lambdas that expect the list of
+                atoms as a parameter.
+        '''
+        self.__checks.extend(args)
+
+    def remove_checks(self, *args: Sequence[Callable]):
+        '''
+        Remove one or more checks from the ones to apply
+        when validating.
+
+        Parameters:
+            *args : Sequence[Callable]
+                A list of functions or lambdas. They will not
+                be removed if not currently present.
+        '''
+        for x in [a for a in args if a in self.__checks]:
+            self.__checks.remove(x)
+
+    def validate(self, atoms: Sequence) -> Mapping[Callable, Any]:
+        '''
+        Applies all previously provided checks to the given list.
+
+        Parameters:
+            atoms : Sequence
+                The sequence of atoms to validate, will be passed
+                to all previously given checks.
+        Returns:
+            A Mapping of each check to its result.
+        '''
+        return {check : check(atoms) for check in self.__checks}
