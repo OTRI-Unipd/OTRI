dde7f59d66d6caf6743bb0ccc6aa9faa7cfe26cd
Luca Crema <lc.crema@hotmail.com>
2020-05-11 17:51:27 +0200

    test(integrator_filter): copied avg filter tests

---
+++ test/filtering/filters/integrator_filter_test.py
@@ -0,0 +1,38 @@
+from otri.filtering.filters.integrator_filter import IntegratorFilter
+from otri.filtering.stream import Stream
+import unittest
+
+
+class IntegratorFilterTest(unittest.TestCase):
+
+    def test_one_input(self):
+        avg_filter = IntegratorFilter(Stream(), {})
+        self.assertEqual(len(avg_filter.get_input_streams()), 1)
+
+    def test_one_output(self):
+        avg_filter = IntegratorFilter(Stream(), {})
+        self.assertEqual(len(avg_filter.get_output_streams()), 1)
+
+    def test_simple_stream_count(self):
+        # Checking the counting works
+        example_stream = Stream(
+            [{"a": 1}, {"a": 2}, {"a": 3}, {"a": 4}, {"a": 5}, {"b": 6}, {"c": 7}]
+        )
+        example_stream.close()
+        expected = 5
+        avg_filter = IntegratorFilter(example_stream, ("a"))
+        while len(example_stream):
+            avg_filter.execute()
+        self.assertEqual(avg_filter.get_counts()["a"], expected)
+
+    def test_simple_stream_sum(self):
+        # Checking the sum works
+        example_stream = Stream(
+            [{"a": 1}, {"a": 2}, {"a": 3}, {"a": 4}, {"a": 5}, {"b": 6}, {"c": 7}]
+        )
+        example_stream.close()
+        expected = 15
+        avg_filter = IntegratorFilter(example_stream, ("a"))
+        while len(example_stream):
+            avg_filter.execute()
+        self.assertEqual(avg_filter.get_sums()["a"], expected)
