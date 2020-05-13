9e71b44c6d62286581961679dc19ecf1f991cfbb
Luca Crema <lc.crema@hotmail.com>
2020-05-13 23:56:18 +0200

    feat(delta_autocorrelation): + Moved autocorrelation calculation inside a function + Added autocorrelation_delta function that uses deltas ~ Temporarily removed normalization filter

--- autocorrelation.py
+++ autocorrelation.py
@@ -1,38 +1,162 @@
 from otri.filtering.filter_list import FilterList, FilterLayer
 from otri.filtering.stream import Stream
 from otri.filtering.filters.interpolation_filter import InterpolationFilter
-from otri.filtering.filters.phase_filter import PhaseMulFilter
+from otri.filtering.filters.phase_filter import PhaseMulFilter, PhaseDeltaFilter
 from otri.filtering.filters.math_filter import MathFilter
 from otri.filtering.filters.statistics_filter import StatisticsFilter
 from otri.filtering.filters.generic_filter import GenericFilter
 from otri.database.postgresql_adapter import PostgreSQLAdapter, DatabaseQuery
 from otri.config import Config
 from pathlib import Path
+from typing import Mapping, Collection
 import matplotlib.pyplot as plt
 import json
 import time
 
 DATABASE_TABLE = "atoms_b"
 DB_TICKER_QUERY = "data_json->>'ticker' = '{}' AND data_json->>'provider' = 'yahoo finance' ORDER BY data_json->>'datetime'"
-
-
 def query_lambda(ticker): return DB_TICKER_QUERY.format(ticker)
 
 
 RUSSELL_3000_FILE = Path("docs/russell3000.json")
 
+def autocorrelation(input_stream : Stream, atom_keys : Collection)->Mapping:
+    '''
+    Calculates autocorrelation of the given stream.
 
-def on_atom(atom):
-    pass
-    #closes.append(atom['close'])
+    Parameters:
+        input_stream : Stream
+            Stream of atoms from the same ticker ordered by timestamp.
+        atom_keys : Collection
+            Collection of keys to calculate autocorrelation of.
+    Returns:
 
+    '''
+    # Filter list 1
 
-def on_finished():
-    pass
-    #plt.plot(closes)
-    #plt.xticks(rotation=90)
-    #plt.show()
+    tuple_extractor = GenericFilter(
+        source_stream=input_stream,
+        operation=lambda element: element[0]
+    )
+    f_layer_tuple_ex = FilterLayer([tuple_extractor])
 
+    interp_filter = InterpolationFilter(
+        input_stream=tuple_extractor.get_output_stream(0),
+        keys_to_change=atom_keys,
+        target_interval="minutes"
+    )
+    f_layer_interp = FilterLayer([interp_filter])
+
+    stats_filter = StatisticsFilter(
+        input_stream=interp_filter.get_output_stream(0),
+        keys=atom_keys
+    ).calc_avg().calc_max()
+    f_layer_stats = FilterLayer([stats_filter])
+
+    f_list_1 = FilterList([
+        f_layer_tuple_ex,
+        f_layer_interp,
+        f_layer_stats
+    ])
+    f_list_1.execute()
+
+    # Filter list 2
+
+    normalize_filter = MathFilter(
+        input_stream=stats_filter.get_output_stream(0),
+        keys_operations={k: lambda value: value/v
+                            for k, v in stats_filter.get_max().items()}
+    )
+    f_layer_normalize = FilterLayer([normalize_filter])
+
+    subtract_filter = MathFilter(
+        input_stream=stats_filter.get_output_stream(0),
+        keys_operations={k: lambda value: value - (stats_filter.get_avg()[k])#/stats_filter.get_max()[k])
+                            for k in atom_keys}
+    )
+    f_layer_subtract = FilterLayer([subtract_filter])
+
+    mul_filter = PhaseMulFilter(
+        input_stream=subtract_filter.get_output_stream(0),
+        keys_to_change=atom_keys,
+        distance=1
+    )
+    f_layer_mul = FilterLayer([mul_filter])
+
+    integrator_filter = StatisticsFilter(
+        input_stream=mul_filter.get_output_stream(0),
+        keys=atom_keys
+    ).calc_avg()
+    f_layer_integ = FilterLayer([integrator_filter])
+
+    f_list_2 = FilterList([
+        #f_layer_normalize,
+        f_layer_subtract,
+        f_layer_mul,
+        f_layer_integ
+    ])
+
+    f_list_2.execute()
+
+    auto_correlation = integrator_filter.get_avg()
+    return auto_correlation
+
+def autocorrelation_delta(input_stream : Stream, atom_keys : Collection)->Mapping:
+    '''
+    Calculates autocorrelation of the given stream.
+
+    Parameters:
+        input_stream : Stream
+            Stream of atoms from the same ticker ordered by timestamp.
+        atom_keys : Collection
+            Collection of keys to calculate autocorrelation of.
+    Returns:
+
+    '''
+
+    # Filter list 1
+
+    tuple_extractor = GenericFilter(
+        source_stream=input_stream,
+        operation=lambda element: element[0]
+    )
+    f_layer_tuple_ex = FilterLayer([tuple_extractor])
+
+    interp_filter = InterpolationFilter(
+        input_stream=tuple_extractor.get_output_stream(0),
+        keys_to_change=atom_keys,
+        target_interval="minutes"
+    )
+    f_layer_interp = FilterLayer([interp_filter])
+
+    f_list_1 = FilterList([f_layer_tuple_ex, f_layer_interp])
+    f_list_1.execute()
+
+    delta_filter = PhaseDeltaFilter(
+        input_stream=interp_filter.get_output_stream(0),
+        keys_to_change=atom_keys,
+        distance=1
+    )
+    f_layer_delta = FilterLayer([delta_filter])
+
+    mul_filter = PhaseMulFilter(
+        input_stream=delta_filter.get_output_stream(0),
+        keys_to_change=atom_keys,
+        distance=1
+    )
+    f_layer_mul = FilterLayer([mul_filter])
+
+    avg_filter = StatisticsFilter(
+        input_stream=mul_filter.get_output_stream(0),
+        keys=atom_keys
+    ).calc_avg()
+    f_layer_integ = FilterLayer([avg_filter])
+
+    f_list_2 = FilterList([f_layer_delta,f_layer_mul,f_layer_integ])
+    f_list_2.execute()
+
+    auto_correlation = avg_filter.get_avg()
+    return auto_correlation
 
 KEYS_TO_CHANGE = ("open", "high", "low", "close")
 
@@ -45,90 +169,10 @@ if __name__ == "__main__":
     tickers_dict = json.load(RUSSELL_3000_FILE.open("r"))
     tickers = [ticker['ticker'] for ticker in tickers_dict['tickers']]
     for ticker in tickers:
-        db_stream = db_adapter.stream(DatabaseQuery(
+        db_stream_1 = db_adapter.stream(DatabaseQuery(
+            DATABASE_TABLE, query_lambda(ticker)))
+        db_stream_2 = db_adapter.stream(DatabaseQuery(
             DATABASE_TABLE, query_lambda(ticker)))
 
-        closes = list()
-        start_time = time.time()
-
-        # Filter list 1
-
-        tuple_extractor = GenericFilter(
-            source_stream=db_stream,
-            operation=lambda element: element[0]
-        )
-        f_layer_tuple_ex = FilterLayer([tuple_extractor])
-
-        interp_filter = InterpolationFilter(
-            input_stream=tuple_extractor.get_output_stream(0),
-            keys_to_change=KEYS_TO_CHANGE,
-            target_interval="minutes"
-        )
-        f_layer_interp = FilterLayer([interp_filter])
-
-        stats_filter = StatisticsFilter(
-            input_stream=interp_filter.get_output_stream(0),
-            keys=KEYS_TO_CHANGE
-        ).calc_avg().calc_max()
-        f_layer_stats = FilterLayer([stats_filter])
-
-        f_list_1 = FilterList([
-            f_layer_tuple_ex,
-            f_layer_interp,
-            f_layer_stats
-        ])
-        f_list_1.execute()
-
-        # Filter list 2
-
-        normalize_filter = MathFilter(
-            input_stream=stats_filter.get_output_stream(0),
-            keys_operations={k: lambda value: value/v
-                             for k, v in stats_filter.get_max().items()}
-        )
-        f_layer_normalize = FilterLayer([normalize_filter])
-
-        f_list_2 = FilterList([
-            f_layer_normalize
-        ])
-        f_list_2.execute()
-
-        subtract_filter = MathFilter(
-            input_stream=normalize_filter.get_output_stream(0),
-            keys_operations={k: lambda value: value - (stats_filter.get_avg()[k]/stats_filter.get_max()[k])
-                             for k in KEYS_TO_CHANGE}
-        )
-        f_layer_subtract = FilterLayer([subtract_filter])
-
-        f_list_3 = FilterList([
-            f_layer_subtract
-        ])
-        f_list_3.execute()
-
-        mul_filter = PhaseMulFilter(
-            input_stream=subtract_filter.get_output_stream(0),
-            keys_to_change=KEYS_TO_CHANGE,
-            distance=3
-        )
-        f_layer_mul = FilterLayer([mul_filter])
-
-        integrator_filter = StatisticsFilter(
-            input_stream=mul_filter.get_output_stream(0),
-            keys=KEYS_TO_CHANGE
-        ).calc_avg()
-        f_layer_integ = FilterLayer([integrator_filter])
-
-        f_list_4 = FilterList([
-            f_layer_mul,
-            f_layer_integ
-        ])
-
-        f_list_4.execute(on_atom, on_finished)
-
-        end_time = time.time()
-        seconds = end_time - start_time
-        print("it took {} seconds".format(seconds))
-
-        auto_correlation = integrator_filter.get_avg()
-
-        print("{} auto-correlation: {}".format(ticker,auto_correlation.items()))
+        print("{} auto-correlation: {}".format(ticker,autocorrelation(db_stream_1, KEYS_TO_CHANGE).items()))
+        print("{} auto-correlation delta: {}".format(ticker,autocorrelation_delta(db_stream_2, KEYS_TO_CHANGE).items()))
