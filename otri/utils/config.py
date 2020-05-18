9f8c770be1e59622b5d99ea7d7d87f42970455aa
Luca Crema <lc.crema@hotmail.com>
2020-05-18 23:17:00 +0200

    feat(config,py): now keeps a dict of opened config files for quicker reads

--- otri/utils/config.py
+++ otri/utils/config.py
@@ -1,18 +1,26 @@
 import json
 from pathlib import Path
 
+open_files = dict()
+
 def get_value(key: str, filename: str = "config"):
     '''
     Reads config file and looks for the given key.
 
     Parameters:
         key : str
-            Requested configuration key
+            Requested configuration key.
+        filename : str
+            Name of a json file. Must not contain the file extension.
     Returns:
-        str containing the value if the key was found in the config file, None otherwise
+        str containing the value if the key was found in the given config file, None otherwise.
     '''
-    try:
-        with Path("{}.json".format(filename)).open("r") as config_file:
-            return json.load(config_file).get(key, None)
-    except FileNotFoundError:
-        return None
+    if(open_files.get(filename, None) == None):
+        try:
+            open_files[filename] = Path("{}.json".format(filename)).open("r")
+        except FileNotFoundError:
+            return None
+    
+    with open_files[filename] as config_file:
+        return json.load(config_file).get(key, None)
+       
