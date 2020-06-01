76c87b450c0d9ce81415aaa51b56085d25c33dc6
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-06-01 22:41:17 +0200

    feat(filter_layer.py): draft of layer execution policies

--- otri/filtering/filter_layer.py
+++ otri/filtering/filter_layer.py
@@ -13,3 +13,30 @@ class FilterLayer(list):
             if not fil.is_finished():
                 return False
         return True
+
+    @staticmethod
+    def EXEC_ALL(layer):
+        for fil in layer:
+            if not fil.is_finished():
+                return 0
+        return +1
+
+    @staticmethod
+    def EXEC_ONE_AND_PASS(layer):
+        return +1
+
+    @staticmethod
+    def BACK_IF_NO_OUTPUT(layer):
+        for fil in layer:
+            if fil.has_output_anything():
+                return 0
+            elif not fil.is_finished():
+                return -1
+        return +1
+
+    @staticmethod
+    def EXEC_UNTIL_OUTPUT(layer):
+        for fil in layer:
+            if fil.has_output_anything():
+                return +1
+        return 0
