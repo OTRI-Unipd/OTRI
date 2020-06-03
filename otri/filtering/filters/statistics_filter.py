86272a5088aca38f812facda0fe8927bd2078500
Luca Crema <lc.crema@hotmail.com>
2020-06-03 18:38:16 +0200

    feat(stats_filter): added status initialization

--- otri/filtering/filters/statistics_filter.py
+++ otri/filtering/filters/statistics_filter.py
@@ -49,6 +49,8 @@ class StatisticsFilter(Filter):
         self.__input_iter = iter(inputs[0])
         self.__output = outputs[0]
         self.__status = status
+        for stat_name in self.__ops.values():
+            status[stat_name] = dict()
 
     def execute(self):
         '''
@@ -59,8 +61,8 @@ class StatisticsFilter(Filter):
             return
         if self.__input_iter.has_next():
             atom = next(self.__input_iter)
-            for op in self.__ops.keys():
-                op(atom)
+            for op, stat_name in self.__ops.items():
+                op(atom, stat_name)
             self.__output.append(atom)
         # Check that we didn't just pop the last item
         elif self.__input.is_closed():
@@ -129,47 +131,45 @@ class StatisticsFilter(Filter):
         self.__ops[self.__min] = status_name
         return self
 
-    def __sum(self, atom: Mapping):
+    def __sum(self, atom: Mapping, stat_name : str):
         '''
         Update the sum state for the given keys.
         '''
         for k in self.__keys:
             if k in atom.keys():
-                status[self.__ops[self.__sum]][k] = self.__status.setdefault(
-                    self.__ops[self.__sum], dict()).setdefault(k,0) + atom[k]
+                status[stat_name][k] = self.__status[stat_name].setdefault(k,0) + atom[k]
 
-    def __count(self, atom: Mapping):
+    def __count(self, atom: Mapping, stat_name : str):
         '''
         Update the count state for the given keys.
         '''
         for k in self.__keys:
             if k in atom.keys():
-                status[self.__ops[self.__count]][k] = self.__status.setdefault(self.__ops[self.__count],dict())\
-                                                            .setdefault(k, 0) + 1
+                status[stat_name][k] = self.__status[stat_name].setdefault(k, 0) + 1
 
-    def __max(self, atom: Mapping):
+    def __max(self, atom: Mapping, stat_name : str):
         '''
         Update the max state for the given keys.
         '''
         for k in self.__keys:
             if k in atom.keys():
-                val = self.__status.setdefault(self.__ops[self.__max],dict()).setdefault(k, '-inf')
-                self.__status[self.__ops[self.__max]][k] = max(val, atom[k])
+                val = self.__status[stat_name].setdefault(k, '-inf')
+                self.__status[stat_name][k] = max(val, atom[k])
 
-    def __min(self, atom: Mapping):
+    def __min(self, atom: Mapping, stat_name : str):
         '''
         Update the min state for the given keys.
         '''
         for k in self.__keys:
             if k in atom.keys():
-                val = self.__status.setdefault(self.__ops[self.__min],dict()).setdefault(k, 'inf')
-                self.__status[self.__ops[self.__min]][k] = min(val, atom[k])
+                val = self.__status[stat_name].setdefault(k, 'inf')
+                self.__status[stat_name][k] = min(val, atom[k])
 
-    def __avg(self, atom : Mapping):
+    def __avg(self, atom : Mapping, stat_name : str):
         '''
         Update the avg state for the given keys.
         '''
         for k in self.__keys:
             if k in atom.keys():
-                if(self.__status.setdefault(self.__ops[self.__count],dict()).setdefault(k,0) != 0):
-                    self.__status.setdefault(self.__ops[self.__avg], dict())[k] = self.__status.setdefault(self.__ops[self.__sum],dict()).setdefault(k,0) / self.__status[self.__ops[self.__count]][k]
+                if(self.__status[self.__ops[self.__count]].setdefault(k,0) != 0):
+                    self.__status[stat_name][k] = self.__status[self.__ops[self.__sum]].setdefault(k,0) / self.__status[self.__ops[self.__count]][k]
