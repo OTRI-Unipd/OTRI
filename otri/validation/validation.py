0eda0da0dc06f106efb97731a86e818be3920f32
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-08-13 22:43:01 +0200

    feat (exceptions.py): Added various Errors and a Warning feat (valchecks.py): Added some checks, those that can be performed by a single function feat (validation.py): Added ContinuityValidator to operate on a Stream's continuity

--- otri/validation/validation.py
+++ otri/validation/validation.py
@@ -1,4 +1,12 @@
-from typing import Sequence, Callable, Mapping, List, Any, Set, Tuple, Final, Optional
+'''
+Module containing Filters aimed to find and point out errors or other problems.
+'''
+
+__author__ = "Riccardo De Zen <riccardodezen98@gmail.com>"
+__version__ = "1.0"
+
+
+from typing import Sequence, Callable, Mapping, List, Any, Set, Tuple, Final, Optional, TypeVar
 from ..filtering.filter import Filter
 from ..filtering.stream import Stream
 from .exceptions import *
@@ -37,7 +45,7 @@ class ValidatorFilter(Filter):
         except Exception as exc:
             self._on_error(data, exc, index)
 
-    def _check(self, data: Mapping) -> Any:
+    def _check(self, data: Mapping):
         '''
         Check a single atom. Must be implemented. Will return the result of the analysis on an atom
         or raise an Error if the atom has one or more problems.
@@ -45,10 +53,8 @@ class ValidatorFilter(Filter):
         Parameters:
             data : Mapping
                 The data to check.
-        Returns:
-            The result for the analysis on the atom. Although it is meant to be used with `ATOM_OK`
-            and `ATOM_HOLD`, any alteration of these return values is ok as long as the filter
-            remains constistent.
+        Raises:
+            Will raise an Exception if there is some problem in the atom.
         '''
         raise NotImplementedError("ValidatorFilter is an abstract class, please extend it.")
 
@@ -287,3 +293,69 @@ class BufferedValidator(MonoValidator):
             self._hold_buffer.append(data)
         else:
             self._push_data(data, index)
+
+
+class ContinuityValidator(MonoValidator):
+
+    '''
+    Single Stream validator class aiming to check whether the values in a Stream are contiguous.
+    The exact meaning of this depends on the implementation, but in general this is what the filter
+    does:
+
+    1. Retrieve an atom.
+    2. If no atom has been seen before, release it, keeping a reference, and retrieve another one.
+    3. Check wether the two atoms are _contiguous_, based on the implementation.
+    4. If they are NOT, mark them both with the thrown exception.
+    5. Replace the reference to the first atom with one to the second and release it.
+    6. Repeat from step one.
+    '''
+
+    def __init__(self, inputs, outputs, key, continuity: Callable):
+        '''
+        Parameters:
+            inputs : str
+                Name for the single input stream.\n
+            outputs : str
+                Name for the single output stream.\n
+            key : str
+                The key for which to check whether the values are continuous.\n
+            continuity : Callable
+                The method or function defining the concept of continuity between two values.
+                Must take the two values as parameters and return a boolean.
+        '''
+        super().__init__(inputs, outputs)
+        self._key = key
+        self._continuity = continuity
+        self._last_atom = None
+
+    def _check(self, data: Mapping):
+        '''
+        Check if two atoms are contiguous.
+
+        Parameters:
+            data : Mapping
+                The data to check.
+        Raises:
+            Will raise an Exception if there is some problem in the atom.
+        '''
+        last = self._last_atom
+        key = self._key
+        if last != None:
+            first = last[key]
+            second = data[key]
+            if not self._continuity(first, second):
+                # Mark the other atom too.
+                error = ContinuityError(key, first, second)
+                self._add_label(self._last_atom, error)
+                self._last_atom = data
+                raise error
+        # Cache this atom for later, but still release it.
+        self._last_atom = data
+
+
+class ParallelValidator(LinearValidator):
+    '''
+    This filter handles finding errors between two Streams, these Streams are read in parallel,
+    popping one atom from each of them and consulting them together.
+    '''
+    pass
