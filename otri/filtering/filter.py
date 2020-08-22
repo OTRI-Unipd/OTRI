48d947a42707b206837ff1db6273a4cea20553a0
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-08-22 14:59:41 +0200

    feat: same number of input and output streams

--- otri/filtering/filter.py
+++ otri/filtering/filter.py
@@ -210,6 +210,21 @@ class ParallelFilter(Filter):
     one atom from each of them and feeds the sequence to `_on_data(data, index)`.
     '''
 
+    def __init__(self, inputs: Sequence[str], outputs: Sequence[str], input_count: int = 0, output_count: int = 0):
+        '''
+        Similar to the parent class. Only difference is the inputs and outputs number must be the same.
+        See `Filter` for details.
+
+        Raises:
+            ValueError : When the number of inputs and outputs is different.
+        '''
+        # Check same amount of inputs and outputs.
+        if input_count != output_count:
+            raise ValueError("The number of input and output Streams must be the same.")
+
+        super().__init__(inputs, outputs, input_count, output_count)
+
+
     def execute(self):
         '''
         This method gets called by the FilterNet when the filter has to manipulate data.
