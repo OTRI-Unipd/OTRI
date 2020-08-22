cbe06fd3fe797cc323b2d61ca4b21a7ef00ce5f0
Luca Crema <lc.crema@hotmail.com>
2020-08-22 16:44:16 +0200

    refactor metadata_upload: - removed work in progress console input - Fixed script description

--- metadata_update.py
+++ metadata_update.py
@@ -5,7 +5,7 @@ Usage:\n
 python metadata_update.py -p <PROVIDER> [--override]
 '''
 
-__autor__ = "Luca Crema <lc.crema@hotmail.com>"
+__author__ = "Luca Crema <lc.crema@hotmail.com>"
 __version__ = "1.1"
 
 import json
@@ -28,13 +28,13 @@ PROVIDERS = {
 if __name__ == "__main__":
 
     cli = CLI(name="metadata_update",
-              description="Script that downloads weekly historical timeseries data.",
+              description="Updates tickers metadata using a provider.",
               options=[
                   CLIValueOpt(
                       short_name="p",
                       long_name="provider",
                       short_desc="Provider",
-                      long_desc="Provider for the historical data.",
+                      long_desc="Provider for the metadata.",
                       required=True,
                       values=list(PROVIDERS.keys())
                   ),
--- metadata_upload.py
+++ metadata_upload.py
@@ -1,12 +1,12 @@
 '''
 Module that can be run to upload new metadata.\n
-If -p is passed it queries the provider for availability.\n
+If -p is passed it queries the provider for availability and other metadata.\n
 
 Usage:\n
-python metadata_upload.py [-f <FILE>] [-p <PROVIDER>] [--override] [-c --console-input]
+python metadata_upload.py [-f <FILE>] [-p <PROVIDER>] [--override]
 '''
 
-__autor__ = "Luca Crema <lc.crema@hotmail.com>"
+__author__ = "Luca Crema <lc.crema@hotmail.com>"
 __version__ = "1.0"
 
 import json
@@ -36,10 +36,10 @@ def upload_with_provider(provider, db_adapter: DatabaseAdapter, metadata_table, 
     '''
     info = source.info([atom['ticker']])
     if info is False or len(info) == 0:
-        log.i("{} not supported".format(atom['ticker']))
+        log.i("{} not supported by provider".format(atom['ticker']))
     else:
         # Extend the atom with new metadata (that will probably override old metadata)
-        log.i("Extended {} metadata with provider data: {}".format(atom['ticker'], info[0]))
+        log.i("extended {} metadata with provider data".format(atom['ticker']))
         atom.update(info[0])
     # Upload data in DB
     upload_data(db_adapter=db_adapter, metadata_table=metadata_table, atom=atom, override=override)
@@ -66,41 +66,34 @@ def upload_data(db_adapter: DatabaseAdapter, metadata_table, atom: dict, overrid
 if __name__ == "__main__":
 
     cli = CLI(name="metadata_update",
-              description="Script that downloads weekly historical timeseries data.",
+              description="Uploads ticker lists and optionally updates their metadata using one provider.",
               options=[
-                  CLIValueOpt(
-                      short_name="p",
-                      long_name="provider",
-                      short_desc="Provider",
-                      long_desc="Provider for the historical data.",
-                      required=False,
-                      values=list(PROVIDERS.keys())
-                  ),
                   CLIValueOpt(
                       short_name="f",
                       long_name="file",
                       short_desc="File",
                       long_desc="File containing metatdata.",
-                      required=False,
+                      required=True,
                       values=list(JSON_FILES)
+                  ),
+                  CLIValueOpt(
+                      short_name="p",
+                      long_name="provider",
+                      short_desc="Provider",
+                      long_desc="Provider for the metadata.",
+                      required=False,
+                      values=list(PROVIDERS.keys())
                   ),
                   CLIFlagOpt(
                       long_name="override",
                       short_desc="Override DB data",
-                      long_desc="If a duplicate key is found the DB data will be overridden by new downloaded data."
-                  ),
-                  CLIFlagOpt(
-                      short_name="c",
-                      long_name="console-input",
-                      short_desc="Use console input",
-                      long_desc="Uses console input as metadata source. If -f is provided it will upload the file first."
+                      long_desc="If a duplicate key is found the DB data will be overridden by new data."
                   )
               ])
     values = cli.parse()
     provider = values['-p']
     meta_file = values['-f']
     override = values['--override']
-    console_input = values['-c']
 
     # Setup database connection
     db_adapter = PostgreSQLAdapter(
@@ -129,6 +122,3 @@ if __name__ == "__main__":
         else:
             for atom in file_atoms:
                 upload_data(db_adapter, metadata_table, atom, override)
-                # Read console data if required
-
-    db_adapter.close()
