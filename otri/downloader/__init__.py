ffaba459fe8eede81e03352adce6f3686bc4a8a7
Luca Crema <lc.crema@hotmail.com>
2020-08-31 00:13:24 +0200

    feat: probably completed timeseries download generalization - implemented it in yahoo timeseries download

--- otri/downloader/__init__.py
+++ otri/downloader/__init__.py
@@ -1,11 +1,12 @@
 
 __author__ = "Luca Crema <lc.crema@hotmail.com>, Riccardo De Zen <riccardodezen98@gmail.com>"
-__version__ = "1.2"
+__version__ = "2.0"
 
 from datetime import date, timedelta, datetime
 from queue import Queue
-from typing import Any, Mapping, Sequence, Union
-from ..utils import time_handler as th
+from typing import Any, Mapping, Sequence, Union, Callable
+from ..utils import logger as log, key_handler as kh, time_handler as th
+from time import sleep
 
 # All downloaders
 ATOMS_KEY = "atoms"
@@ -26,6 +27,17 @@ META_OPTION_VALUE_TYPE = "option"
 
 # Realtime trade downloader
 META_RT_VALUE_TYPE = "trade"
+
+
+class Intervals:
+    ONE_MINUTE = None
+    TWO_MINUTES = None
+    FIVE_MINUTES = None
+    TEN_MINUTES = None
+    FIFTEEN_MINUTES = None
+    THIRTY_MINUTES = None
+    ONE_HOUR = None
+    ONE_DAY = None
 
 
 class DownloadLimiter:
@@ -67,15 +79,15 @@ class DefaultDownloadLimiter(DownloadLimiter):
     Handles the provider requests limitations by setting a maximum request amount per timedelta (minutes, hours, days, ...).
     '''
 
-    def __init__(self, requests_number: int, timespan: timedelta):
+    def __init__(self, requests: int, timespan: timedelta):
         '''
         Parameters:\n
-            requests_number : int
+            requests : int
                 Number of requests that can be made per timespan.\n
             timespan : timedelta
                 Amount of time where the limit is defined.\n
         '''
-        self.max_requests = requests_number
+        self.max_requests = requests
         self.timespan = timespan
         self.next_reset = datetime(2000, 1, 1)
         self.request_counter = 0
@@ -85,8 +97,8 @@ class DefaultDownloadLimiter(DownloadLimiter):
         Called when performing a request. Updates the requests number.
         '''
         # If enough time has passed we can reset the counter.
-        if(datetime.now() > self.next_reset):
-            self.next_reset = datetime.now() + self.timespan
+        if(datetime.utcnow() > self.next_reset):
+            self.next_reset = datetime.utcnow() + self.timespan
             self.request_counter = 0
         # Update the counter
         self.request_counter += 1
@@ -97,6 +109,7 @@ class DefaultDownloadLimiter(DownloadLimiter):
         Returns:\n
             The amount of sleep time in seconds. 0 if no sleep time is needed.
         '''
+        log.i("c:{} max:{}".format(self.request_counter, self.max_requests))
         if(self.request_counter < self.max_requests):
             return 0
         return (self.next_reset - datetime.utcnow()).total_seconds()
@@ -107,7 +120,14 @@ class Downloader:
     Defines an interface with a data provider of any kind.
     '''
 
-    def __init__(self, provider_name):
+    aliases = {
+        'datetime': None
+    }
+
+    # Default class limiter, can be used to avoid keeping track of provider specific parameters.
+    DEFAULT_LIMITER = DownloadLimiter()
+
+    def __init__(self, provider_name: str):
         '''
         Parameters:\n
             provider_name : str
@@ -117,18 +137,20 @@ class Downloader:
 
     def _set_aliases(self, aliases: Mapping[str, str]):
         '''
+        Extends the current aliases dictionary with new aliases overriding current ones.\n
+
         Parameters:\n
             aliases : Mapping[str, str]
-                Key-value pairs that define the renaming of atoms' keys. Values must be all lowecased.
+                Key-value pairs that define the renaming of atoms' keys. Values must be all lowecased.\n
         '''
-        self.aliases = aliases
+        self.aliases.update(aliases)
 
     def _set_max_attempts(self, max_attempts: int):
         '''
-        Parameters:
+        Parameters:\n
             max_attempts : int
                 Number of maximum attempts the downloader will do to download data. Does not include the data elaboration,
-                if something goes wrong when working on downloaded data the script won't attempt to download it again.
+                if something goes wrong when working on downloaded data the script won't attempt to download it again.\n
         '''
         self.max_attempts = max_attempts
 
@@ -136,7 +158,7 @@ class Downloader:
         '''
         Parameters:\n
             limiter : DownloadLimiter
-                Limiter object that handles the provider request limitations.
+                Limiter object that handles the provider request limitations.\n
         '''
         self.limiter = limiter
 
@@ -147,19 +169,42 @@ class TimeseriesDownloader(Downloader):
     The download should be performed only once and not continuosly.
     '''
 
-    def history(self, ticker: str, start: date, end: date, interval: str) -> Union[dict, bool]:
+    aliases = {
+        'close': None,
+        'open': None,
+        'high': None,
+        'low': None,
+        'adjusted close': None,
+        'volume': None,
+        'datetime': None
+    }
+
+    def __init__(self, provider_name: str, intervals: Intervals):
+        '''
+        Parameters:\n
+            provider_name : str
+                Name of the provider, will be used when storing data in the db.\n
+            intervals : Intervals
+                Defines supported intervals and their aliases for the request. It should extend the otri.downloader.Intervals class.\n
+        '''
+        super().__init__(provider_name=provider_name)
+        self.intervals = intervals
+        self.request_dateformat = "%Y-%m-%d %H:%M"
+        self.datetime_formatter = lambda datetime: th.datetime_to_str(th.str_to_datetime(datetime))
+
+    def history(self, ticker: str, start: datetime, end: datetime, interval: Intervals) -> Union[dict, bool]:
         '''
         Downloads time-series data for a single ticker given two dates.\n
 
        Parameters:\n
             ticker : str
                 The simbol to download data of.\n
-            start : date
+            start : datetime
                 Must be before end.\n
-            end : date
+            end : datetime
                 Must be after and different from start.\n
-            interval : str
-                Could be "1m", "2m", "5m", "15m", "30m", "90m", "60m", "1h", "1d", "5d", "1wk"\n
+            interval : Intervals
+                Can be an enum from any class that extends Intervals. See 'intervals' attribute for possible values.\n
         Returns:\n
             False if there as been an error.\n
             A dictionary containing "metadata" and "atoms" otherwise.\n
@@ -176,8 +221,125 @@ class TimeseriesDownloader(Downloader):
                 - close\n
                 - volume\n
         '''
-        raise NotImplementedError(
-            "This is an abstract method, please implement it in a class")
+        if interval is None:
+            raise Exception("Interval not supported by {}".format(self.provider_name))
+
+        data = dict()
+        # Attempt to download and parse data a number of times that is max_attempts
+        attempts = 0
+        while(attempts < self.max_attempts):
+            try:
+                # Check if there's any wait time to do
+                wait_time = self.limiter.waiting_time()
+                if wait_time > 0:
+                    log.w("exceeded download rates, waiting {} seconds before performing request".format(wait_time))
+                    sleep(wait_time)
+                # Request data as a list of atoms
+                atom_list = self._request(ticker, start.strftime(self.request_dateformat),
+                                          end.strftime(self.request_dateformat), interval)
+                break
+            except Exception as err:
+                attempts += 1
+                log.w("{} - error downloading {} on attempt {}: {}".format(self.provider_name, ticker, attempts, err))
+
+        # Chech if it reached the maximum number of attempts
+        if(attempts >= self.max_attempts):
+            log.e("giving up download of {}, reached max attempts".format(ticker))
+            return False
+
+        # If no data is downloaded the ticker couldn't be found or there has been an error, we're not creating any output.
+        if atom_list is None or not atom_list:
+            log.w("empty downloaded data {}: {}".format(ticker, atom_list))
+            return False
+
+        # Optional atoms preprocessing
+        preprocessed_atoms = self._pre_process(atom_list)
+        # Process atoms keys using aliases and datetime formatter
+        prepared_atoms = []
+        for atom in preprocessed_atoms:
+            new_atom = {}
+            # Renaming
+            for key, value in self.aliases.items():
+                if value is not None:
+                    new_atom[key] = atom[value]
+            # Datetime formatting
+            try:
+                new_atom['datetime'] = self.datetime_formatter(new_atom['datetime'])
+            except KeyError:
+                log.w("missing atoms datetime: {}".format(new_atom))
+            prepared_atoms.append(new_atom)
+
+        # Further optional subclass processing
+        postprocessed_atoms = self._post_process(prepared_atoms)
+        # Append atoms to the output
+        data[ATOMS_KEY] = postprocessed_atoms
+        # Create metadata and append it to the output
+        data[METADATA_KEY] = {
+            META_KEY_TICKER: ticker,
+            META_KEY_INTERVAL: interval,
+            META_KEY_PROVIDER: self.provider_name,
+            META_KEY_TYPE: META_TS_VALUE_TYPE
+        }
+        return data
+
+    def _set_request_timeformat(self, request_dateformat: str):
+        '''
+        By default request timeformat is '%Y-%m-%d %H:%M'.\n
+
+        Parameters:
+            request_dateformat : str
+                String format passed to datetime.strptime before giving the date to the request method.\n
+        '''
+        self.request_dateformat = request_dateformat
+
+    def _set_datetime_formatter(self, formatter: Callable):
+        '''
+        Sets a different datetime formatter for atoms than the default one.
+
+        Parameters:
+            datetime_formatter : str
+                Method that takes a datetime string as parameter and returns a properly formatted YYYY-MM-DD HH:mm:ss.fff datetime string.\n
+        '''
+        self.datetime_formatter = formatter
+
+    def _request(self, ticker: str, start: date, end: date, interval: str) -> list:
+        '''
+        Method that requires data from the provider and transform it into a list of atoms.\n
+        It should call the limiter._on_request and limiter._on_response methods if there is anything the limiter needs to know.\n
+        Should NOT handle exceptions as they're catched in the superclass.\n
+
+        Parameters:\n
+            ticker : str
+                The simbol to download data of.\n
+            start : date
+                Must be before end.\n
+            end : date
+                Must be after and different from start.\n
+            interval : str
+                Its possible values depend on the intervals attribute.
+        '''
+        raise NotImplementedError("This is an abstract method, please implement it in a class")
+
+    def _pre_process(self, atoms: Sequence[Mapping]) -> Sequence[Mapping]:
+        '''
+        Optional metod to pre-process data before aliasing and date formatting.\n
+        Atoms processing should be done here rather than in request because if it fails it won't try another attempt,
+         because the error is not in the download but in the processing.\n
+
+        Parameters:\n
+            atoms : Sequence[Mapping]
+                atoms downloaded and alised.\n
+        '''
+
+    def _post_process(self, atoms: Sequence[Mapping]) -> Sequence[Mapping]:
+        '''
+        Optional method to further process atoms after all the earlier processes like aliasing and date formatting.\n
+
+        Parameters:\n
+            atoms : Sequence[Mapping]
+                atoms downloaded and alised.\n
+        '''
+        return atoms
 
 
 class OptionsDownloader(Downloader):
--- otri/downloader/yahoo_downloader.py
+++ otri/downloader/yahoo_downloader.py
@@ -7,7 +7,7 @@ __version__ = "3.0"
 
 import html
 import json
-from datetime import date
+from datetime import date, timedelta
 from typing import Sequence, Union
 
 import yfinance as yf
@@ -19,156 +19,76 @@ from . import (ATOMS_KEY, META_KEY_DOWNLOAD_DT, META_KEY_EXPIRATION,
                META_KEY_INTERVAL, META_KEY_OPTION_TYPE, META_KEY_PROVIDER,
                META_KEY_TICKER, META_KEY_TYPE, META_OPTION_VALUE_TYPE,
                META_TS_VALUE_TYPE, METADATA_KEY, OptionsDownloader,
-               TimeseriesDownloader)
+               TimeseriesDownloader, DefaultDownloadLimiter, Intervals)
+
+
+PROVIDER_NAME = "yahoo finance"
+
+
+class YahooIntervals(Intervals):
+    ONE_MINUTE = "1m"
+    TWO_MINUTES = "2m"
+    FIVE_MINUTES = "5m"
+    FIFTEEN_MINUTES = "15m"
+    THIRTY_MINUTES = "30m"
+    ONE_HOUR = "1h"
+    ONE_DAY = "1d"
 
 
 class YahooTimeseries(TimeseriesDownloader):
     '''
     Used to download historical time series data from YahooFinance.\n
-
-    Output atom format:
-    {
-        Open,
-        Close,
-        High,
-        Low,
-        Adj Close,
-        Volume,
-        Datetime,
-        provider,
-        ticker,
-        interval
-    }
     '''
 
-    META_VALUE_PROVIDER = "yahoo finance"
+    # Limiter with pre-setted variables
+    DEFAULT_LIMITER = DefaultDownloadLimiter(requests=4, timespan=timedelta(seconds=1))
 
-    # Values to round
-    FLOAT_KEYS = [
-        "Open",
-        "Close",
-        "High",
-        "Low",
-        "Adj Close"
-    ]
+    ts_aliases = {
+        'close': 'Close',
+        'open': 'Open',
+        'high': 'High',
+        'low': 'Low',
+        'adjusted close': 'Adj Close',
+        'volume': 'Volume',
+        'datetime': 'Datetime'
+    }
 
-    def history(self, ticker: str, start: date, end: date, interval: str = "1m", max_attempts: int = 5) -> Union[dict, bool]:
+    def __init__(self, limiter: DefaultDownloadLimiter):
         '''
-        Downloads quote data for a single ticker given two dates.\n
+        Parameters:\n
+            limiter : DefaultDownloadLimiter
+                A limiter object, should be shared with other downloaders too in order to work properly.\n
+        '''
+        super().__init__(provider_name=PROVIDER_NAME, intervals=YahooIntervals)
+        self._set_max_attempts(max_attempts=2)
+        self._set_limiter(limiter=limiter)
+        self._set_request_timeformat("%Y-%m-%d")
+        self._set_aliases(YahooTimeseries.ts_aliases)
 
-       Parameters:\n
+    def _request(self, ticker: str, start: str, end: str, interval: str = "1m"):
+        '''
+        Method that requires data from the provider and transform it into a list of atoms.\n
+        Calls limiter._on_request to update the calls made.
+        Parameters:\n
             ticker : str
                 The simbol to download data of.\n
-            start : date
-                Must be before end.\n
-            end : date
-                Must be after and different from start.\n
+            start : str
+                Download start date.\n
+            end : str
+                Download end date.\n
             interval : str
-                Could be "1m", "2m", "5m", "15m", "30m", "90m", "60m", "1h", "1d", "5d", "1wk"\n
-        Returns:\n
-            False if there as been an error.\n
-            A dictionary containing "metadata" and "atoms" otherwise.\n
-
-            "metadata" contains at least:\n
-                - ticker\n
-                - interval\n
-                - provider\n
-            "atoms" contains at least:\n
-                - datetime (format Y-m-d H:m:s.ms)\n
-                - open\n
-                - close\n
-                - volume\n
+                Its possible values depend on the intervals attribute.
         '''
-        attempts = 0
-        while(attempts < max_attempts):
-            try:
-                # yf_data is type of pandas.Dataframe
-                yf_data = yf.download(ticker, start=YahooTimeseries.__yahoo_time_format(start), end=YahooTimeseries.__yahoo_time_format(
-                    end), interval=interval, round=False, progress=False, prepost=True)
-                break
-            except Exception as err:
-                attempts += 1
-                log.w("there has been an error downloading {} on attempt {}: {}\nTrying again...".format(ticker, attempts, err))
-
-        if(attempts >= max_attempts):
-            log.e("unable to download {}".format(ticker))
-            return False
-        # If no data is downloaded the ticker couldn't be found or there has been an error, we're not creating any output.
-        if yf_data is None or yf_data.empty:
-            log.w("empty downloaded data {}".format(ticker))
-            return False
-
-        return YahooTimeseries.__prepare_data(yf_data, ticker, interval)
-
-    @staticmethod
-    def __yahoo_time_format(date: date):
-        '''
-        Formats time into yfinance-ready string format dates.\n
-        Parameters:\n
-            date : datetime\n
-                Datetime to be formatted for yfinance start and end times.\n
-        '''
-        return date.strftime("%Y-%m-%d")
-
-    @staticmethod
-    def __prepare_data(yf_data, ticker: str, interval: str) -> Union[dict, bool]:
-        '''
-        Standardizes timeseries data.\n
-
-        Parameters:\n
-            yf_data : pandas.Dataframe\n
-                Downloaded historical data.\n
-            ticker : str\n
-                Name of the downloaded ticker.\n
-            interval : str\n
-                Amount of time between downloaded atoms.\n
-        Returns:\n
-            Standardized data dict or False if an error occurred.\n
-        '''
-        # Conversion from dataframe to dict
-        json_data = json.loads(yf_data.to_json(orient="table"))
-        # Format datetime and round numeric values
-        data = {}
-        try:
-            rounded_values = key_handler.round_shallow(data=YahooTimeseries.__format_datetime(
-                json_data["data"]), keys=YahooTimeseries.FLOAT_KEYS)
-        except Exception as e:
-            log.w("invalid downloaded data, could not round values: {}".format(e))
-            return False
-        data[ATOMS_KEY] = rounded_values
-        # Addition of metadata
-        data[METADATA_KEY] = {
-            META_KEY_TICKER: ticker,
-            META_KEY_INTERVAL: interval,
-            META_KEY_PROVIDER: YahooTimeseries.META_VALUE_PROVIDER,
-            META_KEY_TYPE: META_TS_VALUE_TYPE,
-            META_KEY_DOWNLOAD_DT: th.now()
-        }
-
-        return data
-
-    @staticmethod
-    def __format_datetime(atoms: list) -> list:
-        '''
-        Standardizes datetime format.\n
-
-        Parameters:\n
-            atoms : list
-                list of downloaded atoms, keys must be already lowercased\n
-        Returns:\n
-            List of atoms with standardized datetime.\n
-        '''
-        for atom in atoms:
-            try:
-                atom['Datetime'] = th.datetime_to_str(th.str_to_datetime(atom['Datetime']))
-            except KeyError as err:
-                log.e("Error in datetime format: {}, atom: {}".format(err, atom))
-        return atoms
+        self.limiter._on_request()
+        pandas_table = yf.download(tickers=ticker, start=start, end=end, interval=interval, rounding=True, progress=False, prepost=True)
+        dictionary = json.loads(pandas_table.to_json(orient="table"))
+        return dictionary['data']
 
 
 class YahooOptions(OptionsDownloader):
 
-    META_VALUE_PROVIDER = YahooTimeseries.META_VALUE_PROVIDER
+    def __init__(self):
+        super().__init__(provider_name=PROVIDER_NAME)
 
     def expirations(self, ticker: str) -> Union[Sequence[str], bool]:
         '''
@@ -235,7 +155,7 @@ class YahooOptions(OptionsDownloader):
         # Append medatada values
         chain[METADATA_KEY] = {
             META_KEY_TICKER: ticker,
-            META_KEY_PROVIDER: YahooTimeseries.META_VALUE_PROVIDER,
+            META_KEY_PROVIDER: self.provider_name,
             META_KEY_OPTION_TYPE: kind,
             META_KEY_EXPIRATION: expiration,
             META_KEY_TYPE: META_OPTION_VALUE_TYPE
@@ -396,6 +316,6 @@ class YahooMetadata:
             except Exception as e:
                 log.e("there has been an exception when retrieving ticker {} ISIN: {}".format(ticker, e))
             # Add provider
-            info['provider'] = [YahooTimeseries.META_VALUE_PROVIDER]
+            info['provider'] = [PROVIDER_NAME]
             data.append(info)
         return data
