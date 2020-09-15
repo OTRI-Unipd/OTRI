ef0d6240fbeed26a0078adf01db0acf7a765e76a
Riccardo De Zen <riccardodezen98@gmail.com>
2020-09-15 02:35:30 +0200

    profiling

--- single_stream_validation.py
+++ single_stream_validation.py
@@ -9,6 +9,7 @@ from otri.utils import logger as log
 from progress.counter import Counter
 from termcolor import colored
 from pathlib import Path
+import cProfile
 import json
 
 RUSSELL_3000_FILE = Path("docs/russell3000.json")
@@ -73,50 +74,55 @@ VALIDATION_PARAMS = [(
 
 
 if __name__ == "__main__":
+    with cProfile.Profile() as pf:
+        db_adapter = PostgreSQLAdapter(
+            database=config.get_value("postgresql_database"),
+            user=config.get_value("postgresql_username"),
+            password=config.get_value("postgresql_password"),
+            host=config.get_value("postgresql_host"),
+            port=config.get_value("postgresql_port", "5432")
+        )
 
-    db_adapter = PostgreSQLAdapter(
-        database=config.get_value("postgresql_database"),
-        user=config.get_value("postgresql_username"),
-        password=config.get_value("postgresql_password"),
-        host=config.get_value("postgresql_host"),
-        port=config.get_value("postgresql_port", "5432")
-    )
+        for params in VALIDATION_PARAMS:
+            analysis = params[0]
+            provider = params[1]
+            get_tickers = params[2]
+            output_file = params[3]
+            manage_results = params[4]
 
-    for params in VALIDATION_PARAMS:
-        analysis = params[0]
-        provider = params[1]
-        get_tickers = params[2]
-        output_file = params[3]
-        manage_results = params[4]
+            results_per_ticker = dict()
+            flags_per_ticker = dict()
+            percent_per_ticker = dict()
 
-        results_per_ticker = dict()
-        flags_per_ticker = dict()
-        percent_per_ticker = dict()
-
-        for ticker in get_tickers():
-            with db_adapter.session() as session:
-                atoms_table = db_adapter.get_classes()[DATABASE_TABLE]
-                query = db_share_query(session, atoms_table, ticker, provider)
-                db_stream = db_adapter.stream(query, batch_size=1000)
-            log.d("Beginning {} for {}".format(analysis.__class__.__name__, ticker))
-            init_counter()
-            result, flagged, total, elapsed_time = analysis.execute([db_stream])
-            close_counter()
-            log.i("Completed in {} seconds.".format(elapsed_time))
-            log.i("Result: {}, Flagged: {} / Total: {}, elapsed_time: {}".format(
-                result, flagged, total, elapsed_time
-            ))
-            if total > 0:
-                results_per_ticker[ticker] = result
-            if flagged > 0:
-                flags_per_ticker[ticker] = flagged
-                percent_per_ticker[ticker] = flagged / total * 100
-
-        with output_file.open("w+") as out:
-            flagged_tickers = len(flags_per_ticker.keys())
-            out.write("{} tickers had flagged values.\n".format(flagged_tickers))
-            if flagged_tickers != 0:
-                out.write("{} average percentage of flags.".format(
-                    sum(percent_per_ticker.values()) / len(results_per_ticker.keys())
+            for ticker in get_tickers()[:10:]:
+                with db_adapter.session() as session:
+                    atoms_table = db_adapter.get_classes()[DATABASE_TABLE]
+                    query = db_share_query(session, atoms_table, ticker, provider)
+                    db_stream = db_adapter.stream(query, batch_size=1000)
+                log.d("Beginning {} for {}".format(analysis.__class__.__name__, ticker))
+                init_counter()
+                result, flagged, total, elapsed_time = analysis.execute([db_stream])
+                close_counter()
+                log.i("Completed in {} seconds.".format(elapsed_time))
+                log.i("Result: {}, Flagged: {} / Total: {}, elapsed_time: {}".format(
+                    result, flagged, total, elapsed_time
                 ))
-            out.write("Results: {}".format(manage_results(results_per_ticker)))
+                if total > 0:
+                    results_per_ticker[ticker] = result
+                    log.i("Execution speed: {} atoms/s".format(total / elapsed_time))
+                if flagged > 0:
+                    flags_per_ticker[ticker] = flagged
+                    percent_per_ticker[ticker] = flagged / total * 100
+
+            with output_file.open("w+") as out:
+                flagged_tickers = len(flags_per_ticker.keys())
+                out.write("{} tickers had flagged values.\n".format(flagged_tickers))
+                if flagged_tickers != 0:
+                    out.write("{} average percentage of flags.".format(
+                        sum(percent_per_ticker.values()) / len(results_per_ticker.keys())
+                    ))
+                out.write("Results: {}".format(manage_results(results_per_ticker)))
+
+        pf.dump_stats("validation.prof")
+
+    # os.system("python -m snakeviz validation.prof")
